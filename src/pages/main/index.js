15c6447faed784b9a106a48c8b115e807d12c64c
Ana Xavier <ana.xavier@m2msolutions.com.br>
2020-01-22 11:26:56 -0300

    modulo3.9

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,4 +1,33 @@
-import React from 'react';
-const Main = () => <h1>Main</h1>;
+import React, {Component, Fragment} from 'react';
 
-export default Main;
+export default class Main extends Component {
+  //nesse caso usa -se para compos mas simples
+  state = {
+    repositoryInput:'',    
+  };
+  render(){
+    return(        
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput:e.target.value})}
+            />
+            <button type="submit">Adicionar</button>
+            <ul>
+              <li>
+                <p>
+                  <strong>facebook/react</strong>
+                  (A declarative, efficient, and flexible JavaScript library for building user interfaces.)
+                </p>
+                <a href="http://github.com/facebook/react">Acessar</a>
+              </li>
+            </ul>
+        </form>
+      </Fragment>
+    ); 
+  }
+}
+
+
